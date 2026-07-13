d0f54e213f07b54c459f50628b515d946152feb6
Guayarasan <zhelxones.gy@gmail.com>
2026-07-12 23:28:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from database import get_connection
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    try:
+        conn = get_connection()
+        conn.close()
+        status = "🟢 Base de datos conectada"
+    except Exception as e:
+        status = f"🔴 Error de conexión: {e}"
+
+    return render_template("index.html", status=status)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
